8a2967abd7673fd403c12cbfd373c9d575734cc5
Javier Ribal del Río <javierribal@gmail.com>
2024-03-20 08:10:39 +0000

    Tema lumen

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Button from 'react-bootstrap/Button';
 import reportWebVitals from './reportWebVitals';
+
+// Boostrap y Bootswach
+import "bootswatch/dist/lumen/bootstrap.min.css";
+
+//
+
+// Componentes
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Button variant='primary' >asdfadsf</Button>
     <App />
   </React.StrictMode>
 );
