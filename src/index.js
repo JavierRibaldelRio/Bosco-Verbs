a0eb54ad62551484b2508820f707b1609f7b2676
Javier Ribal del Río <javierribal@gmail.com>
2024-05-12 23:52:19 +0200

    Google analitics

--- src/index.js
+++ src/index.js
@@ -17,6 +17,36 @@ import IrregularVerbsGame from './pages/Irregular-Verbs-Game';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+// Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+
+const firebaseConfig = {
+
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+
+};
+
+console.log('firebaseConfig :>> ', firebaseConfig);
+
+// Initialize Firebase and analytics
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
