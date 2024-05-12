46cbb94aeeda31b7e5d2c91a5fb74770621046e1
Javier Ribal del Río <javierribal@gmail.com>
2024-05-12 23:53:40 +0200

    Cambiar Class por Clasname

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,17 @@ import { faUser, faFileLines } from '@fortawesome/free-solid-svg-icons';
 
 
 function Footer() {
-    return <div class="container">
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <p class="col-md-4 mb-0 text-body-secondary">© 2024 <a href="https://valenciasjb.salesianos.edu/" target="_blank">Colegio Salesianos San Juan Bosco Valencia</a></p>
+    return <div className="container">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p className="col-md-4 mb-0 text-body-secondary">© 2024 <a href="https://valenciasjb.salesianos.edu/" target="_blank">Colegio Salesianos San Juan Bosco Valencia</a></p>
 
-            <a href="https://getbootstrap.com/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <a href="https://getbootstrap.com/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             </a>
 
-            <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="https://github.com/JavierRibaldelRio/Irregular-Verbs-Test-Tool#" class="nav-link px-2 text-body-secondary">Source Code <FontAwesomeIcon icon={faGithub} /> </a></li>
-                <li class="nav-item"><a href="https://github.com/JavierRibaldelRio/Irregular-Verbs-Test-Tool/issues" class="nav-link px-2 text-body-secondary">Issues <FontAwesomeIcon icon={faFileLines} /></a></li>
-                <li class="nav-item"><a href="https://github.com/JavierRibaldelRio/" class="nav-link px-2 text-body-secondary">Author <FontAwesomeIcon icon={faUser} /></a></li>
+            <ul className="nav col-md-4 justify-content-end">
+                <li className="nav-item"><a href="https://github.com/JavierRibaldelRio/Irregular-Verbs-Test-Tool#" className="nav-link px-2 text-body-secondary">Source Code <FontAwesomeIcon icon={faGithub} /> </a></li>
+                <li className="nav-item"><a href="https://github.com/JavierRibaldelRio/Irregular-Verbs-Test-Tool/issues" className="nav-link px-2 text-body-secondary">Issues <FontAwesomeIcon icon={faFileLines} /></a></li>
+                <li className="nav-item"><a href="https://github.com/JavierRibaldelRio/" className="nav-link px-2 text-body-secondary">Author <FontAwesomeIcon icon={faUser} /></a></li>
 
             </ul>
         </footer>
