849ad4b4cf5527f32c61b777df691ce042867c55
Javier Ribal del Río <javierribal@gmail.com>
2024-06-08 10:34:26 +0200

    Button to dowload Irregular Verbs PDF

--- src/pages/Verbs-List.js
+++ src/pages/Verbs-List.js
@@ -1,15 +1,30 @@
 import React from "react";
+
 import IrregularVerbs from "../data/Irregular-Verbs-String.json";
 import TablaVerbos from "../components/TablaVerbos";
 
-function VerbsList(){
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
+import Button from "react-bootstrap/esm/Button";
+
+
+function VerbsList() {
 
     return <div>
-            <h1 id="titulo-seccion">Irregular Verbs List</h1>
+        <h1 id="titulo-seccion">Irregular Verbs List</h1>
 
-            <TablaVerbos irregularVerbs={IrregularVerbs}/>
-        </div>  
+        <TablaVerbos irregularVerbs={IrregularVerbs} />
+
+        {/* OJO: Falta añadir el enlace a la lista */}
+
+
+        <div className="text-center download-button">
+            <a className="unformated-a" href="/Irregular-Verbs-List.pdf" target="blank" download={true}><Button><FontAwesomeIcon icon={faArrowDown} /> Download List</Button></a>
+        </div>
+
+
+    </div >
 
 
 }
